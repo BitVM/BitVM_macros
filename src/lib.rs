f364a172e353d7eb70b90d5dd485089a5c0f28b9
Lukas <lucidLuckylee@users.noreply.github.com>
2024-03-18 12:05:34 +0100

    Fix: Consider the generic annotations

--- src/lib.rs
+++ src/lib.rs
@@ -5,12 +5,14 @@ use quote::quote;
 use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(LeafGetters)]
-pub fn leaf_functions(input: TokenStream) -> TokenStream {
+pub fn leaf_getters(input: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(input as DeriveInput);
     let name = &ast.ident;
+    let generics = &ast.generics;
+    let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
 
     let gen = quote! {
-        impl LeafGetters for #name {}
+        impl #impl_generics LeafGetters for #name #ty_generics #where_clause {}
     };
 
     gen.into()
